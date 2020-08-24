a14625f49c8924d345fc4d3b0f6ac4afdefb7105
anil <anil@DESKTOP-D0JPL8G>
2020-08-24 22:47:07 +0530

    Zuul Example register Eureka Client

---
+++ Zuul/ZullExample/src/test/java/com/ZullExample/ZullExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ZullExample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZullExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
